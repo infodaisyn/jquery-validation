d4ddad9ae4d90855c629d5b3e246988b6cc2ba39
Daisyn Fernandes <daisyn@sjinnovation.com>
2017-07-03 15:54:55 +0530

    jquery file update

--- validation.js
+++ validation.js
@@ -1,10 +1,72 @@
-$(function() {
-	$.validator.addMethod('strongPassword',function(value,element) {
-		return this.optional(element)
-		|| value.length>=6
-		&& /\d/.test(value)
-		&&/[a-z]/i.test(value);
-	},'your passowrd should have atleast one number and one chararter')
+// $(function() {
+// 	$.validator.addMethod('strongPassword',function(value,element) {
+// 		return this.optional(element)
+// 		|| value.length>=6
+// 		&& /\d/.test(value)
+// 		&&/[a-z]/i.test(value);
+// 	},'your password should have atleast one number and one chararter')
+// 	$("#register-form").validate({
+
+// 		rules: {
+
+// 			email: {
+// 				required: true,
+// 				email: true
+// 			},
+
+// 			password:{
+// 				required: true,
+// 				strongPassword: true
+// 			},
+
+// 			password2: {
+// 				required: true,
+// 				equalTo:"#password"
+// 			},
+
+// 			firstName: {
+// 				required: true,
+// 				nowhitespace: true,
+// 				lettersonly: true
+// 			}
+
+// 		},
+
+// 		messages: {
+
+// 			email: {
+// 				required: 'Please Enter the email',
+// 				email: 'Please enter the valid Email address'
+// 			}
+// 		}
+
+// 	});
+// });
+
+$(function(){
+
+	$.validator.setDefaults({
+    errorClass: 'help-block',
+    highlight: function(element) {
+      $(element)
+        .closest('.form-group')
+        .addClass('has-error');
+    },
+    unhighlight: function(element) {
+      $(element)
+        .closest('.form-group')
+        .removeClass('has-error');
+    },
+    errorPlacement: function (error, element) {
+      if (element.prop('type') === 'checkbox') {
+        error.insertAfter(element.parent());
+      } else {
+        error.insertAfter(element);
+      }
+    }
+  });
+
+
 	$("#register-form").validate({
 
 		rules: {
@@ -14,31 +76,37 @@ $(function() {
 				email: true
 			},
 
-			password:{
-				required: true,
-				strongPassword: true
-			},
-
+			password: "required",
 			password2: {
 				required: true,
-				equalTo:"#password"
+				equalTo: "#password"
 			},
 
 			firstName: {
 				required: true,
 				nowhitespace: true,
 				lettersonly: true
-			}
-
+			} 
 		},
 
 		messages: {
 
 			email: {
-				required: 'Please Enter the email',
-				email: 'Please enter the valid Email address'
-			}
-		}
+				required:  "Please enter email",
+				email: "Please enter valid email address"	
+			},
 
+			password: {
+				required: "Please enter the password",
+				password: " please enter valid password"
+			},
+
+			firstName: {
+				required: "please Enter you First Name",
+				lettersonly: "please enter A-Z letters"
+			}
+
+		}
 	});
+
 });
